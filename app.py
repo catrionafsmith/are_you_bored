8e1d63babf30342f2e0dfb88c4675b603d01c4ee
Katy Smith <113362369+catrionafsmith@users.noreply.github.com>
2023-01-16 21:06:35 +1100

    Update css to add refresh button

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import urllib.request, json, requests
 
 app = Flask(__name__)
 
-@app.route("/index")
+@app.route("/")
 def get_activity():
     response_API = requests.get('http://www.boredapi.com/api/activity/')
     data = response_API.text
     dict = json.loads(data)
 
-    return render_template ("index.html", activity=dict["activity"])
+    return render_template ("home.html", activity=dict["activity"])
 
 if __name__ == '__main__':
     app.run(debug=True)
